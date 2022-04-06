4894d0abc470afe789195328e25265d19c9d3ec3
NJBodam <bodambest@yahoo.com>
2022-04-06 15:42:00 +0100

    first commit

---
+++ src/main/java/com/example/greaterreads/GreaterreadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.greaterreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreaterreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreaterreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greaterreads/GreaterreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greaterreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreaterreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
